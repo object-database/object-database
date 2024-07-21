6c2dc102e3dd8797dd607ae3eb743f4a7e52b66b
JesseNaidoo <156646712+JesseNaidoo@users.noreply.github.com>
2024-07-21 12:07:19 +0200

    api init

---
+++ api/app.js
@@ -0,0 +1,22 @@
+import cors from "cors";
+import bodyParser from "body-parser";
+import express from 'express';
+
+const app = express();
+const PORT = process.env.PORT | 8080;
+
+
+app.use(cors());
+app.use(bodyParser.json());
+app.get("/", (req, res) => {
+    res.send(JSON.stringify({message: "API is Up"}));
+});
+
+
+app.listen(PORT, (error) => {
+    if (!error) {
+        console.log(`Server is running, and App is listening on port ${PORT}`);
+    } else {
+        console.log("Error occurred, server can't start", error);
+    }
+});
