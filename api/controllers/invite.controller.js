8e33379261bf65d99039c39c6be6f707ac0f3381
Bryce <bryce.grahn@bbd.co.za>
2024-07-25 11:37:28 +0200

    Remove users from meeting

--- api/controllers/invite.controller.js
+++ api/controllers/invite.controller.js
@@ -1,12 +1,12 @@
-import {StatusCodes} from "http-status-codes";
-import {realm} from "../utils/realm.js";
-import {Meeting, User} from "../models/classes.model.js";
-import {UserController} from "./index.js";
+import { StatusCodes } from "http-status-codes";
+import { realm } from "../utils/realm.js";
+import { Meeting, User } from "../models/classes.model.js";
+import { UserController } from "./index.js";
 import Realm from "realm";
 
 export const inviteUsers = async (req, res) => {
   try {
-    const {attendees, _meetingId} = req.body;
+    const { attendees, _meetingId } = req.body;
     if (_meetingId || attendees) {
       //Check if meeting exists
       //let meeting = realm.objects(Meeting).find((meeting) => meeting._id === _meetingId);
@@ -33,6 +33,36 @@ export const inviteUsers = async (req, res) => {
       res.status(StatusCodes.BAD_REQUEST).send("Invalid Body");
     }
 
+  } catch (err) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(err.message);
+  }
+}
+
+export const removeUsers = async (req, res) => {
+  try {
+    const { attendees, _meetingId } = req.body;
+    if (_meetingId || attendees) {
+      let id = new Realm.BSON.ObjectID(_meetingId);
+      let meeting = realm.objectForPrimaryKey(Meeting, id);
+      if (meeting) {
+        for (const email of attendees) {
+          const user = await UserController.getUserByEmail(email);
+          realm.write(() => {
+            if (user != null) {
+              const index = meeting.MeetingAttendees.indexOf(user);
+              if (index > -1) {
+                meeting.MeetingAttendees.splice(index, 1); // Remove user from attendees
+              }
+            }
+          })
+        }
+        res.status(StatusCodes.OK).json(meeting);
+      } else {
+        res.status(StatusCodes.BAD_REQUEST).send("Meeting does not exist");
+      }
+    } else {
+      res.status(StatusCodes.BAD_REQUEST).send("Invalid Body");
+    }
   } catch (err) {
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(err.message);
   }
