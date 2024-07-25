78df0897eefa60309257b64a2fb6009f0577ac03
Bryce <bryce.grahn@bbd.co.za>
2024-07-25 14:30:13 +0200

    try catch user api calls

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -8,60 +8,77 @@ export const getUserByEmail = async (email) => {
 }
 
 export const getUser = async (req, res) => {
-  let user;
-  if (req.query.email) {
-    user = realm.objects(User).find((user) => user.email === req.query.email);
-  } else if (req.query.id) {
-    try {
-      let id = new Realm.BSON.ObjectID(req.query.id);
-      user = realm.objectForPrimaryKey(User, id);
-    } catch {
-      res.status(StatusCodes.BAD_REQUEST).json({
-        errorMessage: "ID was malformed",
-      });
-      return;
+  try {
+    let email = req.query.email;
+    if (email) {
+      let user = realm.objects(User).find((user) => user.email === email);
+      if (!user) {
+        res.status(StatusCodes.BAD_REQUEST).json({
+          errorMessage: "This person can't be found",
+        });
+      } else {
+        res.status(StatusCodes.OK).json(user);
+      }
     }
-  } else {
-    res.status(StatusCodes.BAD_REQUEST).json({
-      errorMessage: "No valid parameters provided",
-    });
-    return;
+    else {
+      res.status(StatusCodes.BAD_REQUEST).send("Invalid Body");
+    }
   }
-
-  if (!user) {
-    res.status(StatusCodes.BAD_REQUEST).json({
-      errorMessage: "This person can't be found",
-    });
-  } else {
-    res.status(StatusCodes.OK).json(user);
+  catch (err) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(err.message)
   }
 };
 
 export const createUser = async (req, res) => {
-  if (realm.objects(User).find((user) => user.email === req.body.email)) {
-    res.status(StatusCodes.BAD_REQUEST).json({
-      errorMessage: "This person already exists",
-    });
-  } else {
-    realm.write(() => {
-      let user = realm.create(User, req.body);
-      res.status(StatusCodes.CREATED).json(user);
-    });
+  try {
+    const { email, name } = req.body;
+    if (email || name) {
+      if (realm.objects(User).find((user) => user.email === email)) {
+        res.status(StatusCodes.BAD_REQUEST).json({
+          errorMessage: "This person already exists",
+        });
+      } else {
+        realm.write(() => {
+          let user = realm.create(User, {
+            _id: new Realm.BSON.ObjectId(),
+            email: email,
+            name: name
+          });
+          res.status(StatusCodes.CREATED).json(user);
+        });
+      }
+    }
+    else {
+      res.status(StatusCodes.BAD_REQUEST).send("Invalid Body");
+    }
+  }
+  catch (err) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(err.message)
   }
 };
 
 export const deleteUser = async (req, res) => {
-  let email = req.query.email;
-  let user = realm.objects(User).find((user) => user.email === email);
-  if (!user) {
-    res.status(StatusCodes.BAD_REQUEST).json({
-      errorMessage: "This person doesn't exist",
-    });
-  } else {
-    realm.write(() => {
-      realm.delete(user);
-      res.status(StatusCodes.OK).json();
-    });
+  try {
+    let email = req.query.email;
+    if (email) {
+      let user = realm.objects(User).find((user) => user.email === email);
+      if (!user) {
+        res.status(StatusCodes.BAD_REQUEST).json({
+          errorMessage: "This person doesn't exist",
+        });
+      } else {
+        realm.write(() => {
+          realm.delete(user);
+          res.status(StatusCodes.OK).json();
+        });
+      }
+    }
+    else {
+      res.status(StatusCodes.BAD_REQUEST).send("Invalid Body");
+    }
+  }
+  catch (err) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(err.message)
   }
 };
 
