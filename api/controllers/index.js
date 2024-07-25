2eb1091ce6a3edb2cba85e723bfd0943358fb0f5
Stefan de Jager <stefan.dejager@bbd.co.za>
2024-07-25 09:48:59 +0200

    Meeting and Room endpoints

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -1,4 +1,6 @@
 import * as UserController from './user.controller.js';
 import * as ResetController from './reset.controller.js';
+import * as RoomController from './room.controller.js';
+import * as MeetingController from './meeting.controller.js';
 
-export {UserController, ResetController};
+export {UserController, ResetController, RoomController, MeetingController};
