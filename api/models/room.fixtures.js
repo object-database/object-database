9ce897446292b86e3a9dcc32ba91588c8dcf4abe
JesseNaidoo <156646712+JesseNaidoo@users.noreply.github.com>
2024-07-26 09:11:09 +0200

    Added more timeslots to rooms

--- api/models/room.fixtures.js
+++ api/models/room.fixtures.js
@@ -1,9 +1,34 @@
 import {timeslots} from "./timeslots.fixtures.js";
 
 export const rooms = [
-  {Location: "Cape Town", Name: "CPT Room 1", Capacity: 10, TimeSlots: [timeslots[0], timeslots[1], timeslots[2]]},
-  {Location: "Cape Town", Name: "CPT Room 2", Capacity: 16, TimeSlots: [timeslots[0], timeslots[1]]},
-  {Location: "Rosebank", Name: "JHB Room 1", Capacity: 6, TimeSlots: [timeslots[0], timeslots[1]]},
-  {Location: "Rosebank", Name: "JHB Room 2", Capacity: 20, TimeSlots: [timeslots[0], timeslots[1]]},
-  {Location: "Rosebank", Name: "JHB Colab", Capacity: 100, TimeSlots: [timeslots[0], timeslots[1]]},
+  {
+    Location: "Cape Town",
+    Name: "CPT Room 1",
+    Capacity: 10,
+    TimeSlots: [timeslots[0], timeslots[1], timeslots[2], timeslots[3], timeslots[4], timeslots[5], timeslots[6], timeslots[7], timeslots[8], timeslots[9]]
+  },
+  {
+    Location: "Cape Town",
+    Name: "CPT Room 2",
+    Capacity: 16,
+    TimeSlots: [timeslots[0], timeslots[1], timeslots[2], timeslots[3], timeslots[4], timeslots[5], timeslots[6], timeslots[7], timeslots[8], timeslots[9]]
+  },
+  {
+    Location: "Rosebank",
+    Name: "JHB Room 1",
+    Capacity: 6,
+    TimeSlots: [timeslots[0], timeslots[1], timeslots[2], timeslots[3], timeslots[4], timeslots[5], timeslots[6], timeslots[7], timeslots[8], timeslots[9]]
+  },
+  {
+    Location: "Rosebank",
+    Name: "JHB Room 2",
+    Capacity: 20,
+    TimeSlots: [timeslots[0], timeslots[1], timeslots[2], timeslots[3], timeslots[4], timeslots[5], timeslots[6], timeslots[7], timeslots[8], timeslots[9]]
+  },
+  {
+    Location: "Rosebank",
+    Name: "JHB Colab",
+    Capacity: 100,
+    TimeSlots: [timeslots[0], timeslots[1], timeslots[2], timeslots[3], timeslots[4], timeslots[5], timeslots[6], timeslots[7], timeslots[8], timeslots[9]]
+  },
 ]
